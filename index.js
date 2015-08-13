9fa8dd8d8b6bc81e596c4bc813eb327b3a979348
MircoT <mirco.theone@gmail.com>
2015-08-13 23:32:39 +0200

    Minor changes

--- index.js
+++ index.js
@@ -13,21 +13,19 @@ app.get('/numUsers', function(req, res){
 
 io.on('connection', function(socket){
   
-  numUsers++;
-  io.emit('users num', numUsers);
-
+  io.emit('users num', ++numUsers);
   console.log('a user connected');
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
-
-    numUsers--;
-    io.emit('users num', numUsers);
+    io.emit('users num', --numUsers);
   });
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
+
 });
 
 http.listen(3000, function(){
